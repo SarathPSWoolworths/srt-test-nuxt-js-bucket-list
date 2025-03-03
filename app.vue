c7c9917bba086559839821f6559fb6a76c7dbe7a
Sarath PS <sps@tcs.woolworths.com.au>
2025-03-03 15:53:49 +0530

    initialize Nuxt.js application with basic configuration and structure

---
+++ app.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Bucket List</h1>
+    <input v-model="newItem" @keyup.enter="addItem" placeholder="Add a new item" />
+    <button @click="addItem">Add</button>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <button @click="removeItem(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItem.trim() !== '') {
+        this.items.push(this.newItem);
+        this.newItem = '';
+      }
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+
+input {
+  margin-right: 10px;
+}
+
+button {
+  margin-left: 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
+}
+</style>
